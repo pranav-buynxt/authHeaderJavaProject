7ede1bd2e6f7c920a250809bee45ee0457cb2602
pranav-buynxt <pranav@buynxt.in>
2023-03-26 20:06:45 +0530

    updated

--- src/main/java/com/faiyaz/ondc/authheaderjava/AuthheaderjavaApplication.java
+++ src/main/java/com/faiyaz/ondc/authheaderjava/AuthheaderjavaApplication.java
@@ -2,13 +2,22 @@ package com.faiyaz.ondc.authheaderjava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AuthheaderjavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthheaderjavaApplication.class, args);
 		System.out.println("Program starts from here...");
 	}
+
+	@GetMapping("/")
+	public String check(){
+		return "Working fine";
+	}
+
 
 }
